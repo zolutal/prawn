df4dfe07b28cbc9117a14c7d7907c6add9444497
zolutal <justalmill@gmail.com>
2025-03-02 21:53:07 -0700

    add more byte/int conversion functions

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,28 +2,64 @@
 pub enum Error {
     #[error("Unpacking Error: {0}")]
     UnpackError(String),
-
-    #[error("Packing Error: {0}")]
-    PackError(String),
 }
 
-pub fn u64(bytevec: &[u8]) -> Result<u64, Error> {
+pub fn u64(bytevec: &[u8]) -> u64 {
+    u64::from_le_bytes(bytevec[0..8].try_into().unwrap())
+}
+
+pub fn u32(bytevec: &[u8]) -> u32 {
+    u32::from_le_bytes(bytevec[0..4].try_into().unwrap())
+}
+
+pub fn u16(bytevec: &[u8]) -> u16 {
+    u16::from_le_bytes(bytevec[0..2].try_into().unwrap())
+}
+
+pub fn u8(bytevec: &[u8]) -> u8 {
+    bytevec[0]
+}
+
+pub fn u64_chk(bytevec: &[u8]) -> Result<u64, Error> {
     if bytevec.len() != 8 {
-        return Err(Error::UnpackError("Expected vector of len 8!".into()));
+        return Err(Error::UnpackError("Expected slice of len 8!".into()));
     }
-    let mut val = 0u64;
-    for i in 0..8 {
-        val |= (bytevec[i] as u64) << (i*8);
-    }
-
-    Ok(val)
+    Ok(u64(bytevec))
 }
 
-pub fn p64(val: u64) -> Result<Vec<u8>, Error> {
-    let mut bytevec = vec![0; 8];
-    for i in 0..8 {
-        bytevec[i] = (val >> (i*8)) as u8;
+pub fn u32_chk(bytevec: &[u8]) -> Result<u32, Error> {
+    if bytevec.len() != 4 {
+        return Err(Error::UnpackError("Expected slice of len 4!".into()));
     }
+    Ok(u32(bytevec))
+}
 
-    Ok(bytevec)
+pub fn u16_chk(bytevec: &[u8]) -> Result<u16, Error> {
+    if bytevec.len() != 2 {
+        return Err(Error::UnpackError("Expected slice of len 2!".into()));
+    }
+    Ok(u16(bytevec))
+}
+
+pub fn u8_chk(bytevec: &[u8]) -> Result<u8, Error> {
+    if bytevec.len() != 1 {
+        return Err(Error::UnpackError("Expected slice of len 1!".into()));
+    }
+    Ok(u8(bytevec))
+}
+
+pub fn p64(val: u64) -> Vec<u8> {
+    val.to_le_bytes().to_vec()
+}
+
+pub fn p32(val: u32) -> Vec<u8> {
+    val.to_le_bytes().to_vec()
+}
+
+pub fn p16(val: u16) -> Vec<u8> {
+    val.to_le_bytes().to_vec()
+}
+
+pub fn p8(val: u8) -> Vec<u8> {
+    val.to_le_bytes().to_vec()
 }
